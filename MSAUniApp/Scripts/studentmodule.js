509a11fbb7f2676c80454cdb8d6fc884d4dc0fbe
Victor Chang <thevictorchang@gmail.com>
2015-11-23 18:50:58 +1300

    reverted

--- MSAUniApp/Scripts/studentmodule.js
+++ MSAUniApp/Scripts/studentmodule.js
@@ -1,38 +1,12 @@
 ﻿var StudentModule = (function () {
 
     return {
-
-        getCourses: function (callback) {
-            $.ajax({
-                type: "GET",
-                dataType: "json",
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Courses",
-                success: function(data){
-                    console.log(data);
-                    callback(data);
-                }
-            });
-        },
-
-        addCourse: function (course, callback) {
-
-            $.ajax({
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Courses/",
-                type: "POST",
-                data: course,
-                success: function (data, textStatus, jqXHR) {
-                    callback();
-                }
-            });
-
-        },
-
         getStudents: function (callback) {
              $.ajax({ 
                 type: "GET",
                 dataType: "json",
                 // API url here
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Students",
+                url: "http://msauniapp.azurewebsites.net/api/Students",
                 success: function(data){        
                     console.log(data);
                     callback(data);
@@ -45,7 +19,7 @@
             $.ajax({ 
                 type: "GET",
                 dataType: "json",
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Students/" + id,
+                url: "http://msauniapp.azurewebsites.net/api/Students/" + id,
                 success: function(data){        
                     console.log(data);
                     callback(data);
@@ -57,7 +31,7 @@
         addStudent: function (student, callback) {
              
              $.ajax({
-                 url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Students/",
+                url : "http://msauniapp.azurewebsites.net/api/Students/",
                 type: "POST",
                 data : student,
                 success: function(data, textStatus, jqXHR)
@@ -71,7 +45,7 @@
         updateStudent: function (studentid, student, callback){
             
             $.ajax({
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Students/" + studentid,
+                url : "http://msauniapp.azurewebsites.net/api/Students/" + studentid,
                 type: "PUT",
                 data : student,
                 success: function(data, textStatus, jqXHR)
@@ -86,7 +60,7 @@
             $.ajax({ 
                 type: "DELETE",
                 dataType: "json",
-                url: "http://contosouniversitythevictorchang.azurewebsites.net/api/Students/" + studentid,
+                url: "http://msauniapp.azurewebsites.net/api/Students/" + studentid,
                 success: function(data){        
                     callback();
                 }
